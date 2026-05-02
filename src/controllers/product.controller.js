d69639c22e7a8f18b586d7ac84b573eab1fa0b17
CyberSparkx <narensarkar607@gmail.com>
2026-05-03 01:28:27 +0530

    CRD routes and controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -32,7 +32,58 @@ const getAllProductsController = async (req, res) => {
 
 }
 
+
+
+const getSpecificProductController = async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        const product = await Product.findById(id);
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error',  error : error.message });
+    }
+
+}
+
+const updateProductController = async (req, res) => {
+    const {id} = req.params;
+    const {title,price,description,imageUrl,category} = req.body;
+
+    try {
+
+        const updatedProduct = await Product.findByIdAndUpdate(
+            id,
+            { title, price: { amount: price.amount }, description, imageUrl, category },
+            { new: true }
+        );
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error',  error : error.message });
+    }
+}
+
+const deleteProductController = async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        const deletedProduct = await Product.findByIdAndDelete(id);
+        if (!deletedProduct) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.status(200).json({ message: 'Product deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error',  error : error.message });
+    }
+}
+
 module.exports = {
     createProductController,
-    getAllProductsController
+    getAllProductsController,
+    getSpecificProductController,
+    updateProductController,
+    deleteProductController
 }
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -2,13 +2,19 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/product.model');
 const checkSession = require('../middleware/checkSession.middleware');
-const { createProductController, getAllProductsController } = require('../controllers/product.controller');
+const { createProductController, getAllProductsController, getSpecificProductController, updateProductController, deleteProductController } = require('../controllers/product.controller');
 
 
 router.post('/addProduct', checkSession, createProductController);
 router.get('/getAllProducts', getAllProductsController)
+router.get('/getSpecificProduct/:id', getSpecificProductController);
+router.put('/updateProduct/:id', checkSession, updateProductController);
+router.delete('/deleteProduct/:id', checkSession, deleteProductController);
 
 module.exports = {
     addProduct:router,
-    getAllProducts:router
+    getAllProducts:router,
+    getSpecificProduct:router,
+    updateProduct:router,
+    deleteProduct:router
 }
